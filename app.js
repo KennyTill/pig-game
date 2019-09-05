49911f2e54ce1e28a3471253ccf5ac751cbb091d
Kenneth Till <kenneth.g.till@gmail.com>
2019-09-05 15:34:03 -0700

    initial setup of game

--- app.js
+++ app.js
@@ -30,3 +30,10 @@ function rollDie() {
 document.querySelector(".dice").style.display = "none";
 
 document.querySelector(".btn-roll").addEventListener("click", rollDie);
+
+
+//game setup
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
